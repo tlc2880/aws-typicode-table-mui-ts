544dc5b07ba45de8d9515df362616bab198c34db
tlc2880 <tommylcao@gmail.com>
2023-11-10 10:39:36 -0800

    Add git-5

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import DataTable from './components/DataTable';
 
 function App() {
+
   return (
     <div>
       <DataTable />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
